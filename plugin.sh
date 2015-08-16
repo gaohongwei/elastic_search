bf82384e366feb56ef9f26aa6259a5d136c8c34d
kevin-wei-pan <kwei@paloaltonetworks.com>
2015-08-16 15:12:37 -0700

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -35,3 +35,11 @@ bin/plugin -i shield -u file://PATH_TO_ZIP_FILE
 ./bin/shield/esusers useradd ky -p abc123Abc
 ./bin/shield/esusers list
 ./bin/shield/esusers passwd ky -p abc123AbC
+
+##################### Security  #####################
+作为分布式服务器，一般部署在内网，以服务的形式提供给应用使用。
+而Elasticsearch默认绑定的IP地址是：0.0.0.0，
+也就是说如果这个机子有几个网卡，则elasticsearch都可以通过这些IP来使用其服务。
+所以如果我们的服务器有网卡绑定在外网时一定要注意设置Elasticsearch的属性：
+network.host为内网IP，
+否则的话，别人就可以通过CURL命令来操作我们索引服务器上的索引数据，也自己随意添加索引和删除索引，相当可怕~
