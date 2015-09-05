73b57ac17e407958191332e7c6cd4e69b0c855aa
kevin-wei-pan <kwei@paloaltonetworks.com>
2015-09-04 17:11:18 -0700

    Create es_server.rb

---
+++ es_server.rb
@@ -0,0 +1,11 @@
+#config/initializer/tire.rb
+Tire.configure do
+  url "http://localhost:9200"
+  #you can uncomment the next line if you want to see the elasticsearch queries in their own seperate log
+  #logger "#{Rails.root}/log/es.log" 
+end
+
+
+Tire.configure { url "http://myremoteserver.com:9200" }
+
+# Can I sepcify multiple url?
